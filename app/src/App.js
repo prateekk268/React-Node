a90f9d16eebffb114d8b3459b17cb195dab0e53e
Prateek Kumar <prateekk268@gmail.com>
2022-11-20 12:22:21 +0530

    working

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import Todo from "./components/Todo";
+
+
+function App() {
+  return (
+    <div>
+      <h3>My Todos</h3>
+     <Todo text = "Learn React"/>
+     <Todo text = "Master React"/>
+     <Todo text = "Explore the full React course"/>
+    </div>
+  );
+}
+
+export default App;
